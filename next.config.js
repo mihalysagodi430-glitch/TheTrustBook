8c85671e18f69a7e1e31eea8783a16aff4769191
mihalysagodi430-glitch <mihalysagodi430@gmail.com>
2025-11-05 09:20:40 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,3 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, experimental: { appDir: true }, i18n: { locales: ['en','hu','ja','zh','hi','ru'], defaultLocale: 'en' } }
-module.exports = nextConfig;
