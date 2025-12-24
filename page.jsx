5da9d80eba74a2a35fe82f306adbcbed78f89aee
mihalysagodi430-glitch <mihalysagodi430@gmail.com>
2025-12-24 22:40:38 +0100

    Delete page.jsx

--- page.jsx
+++
@@ -1,4 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-const EbookForm = dynamic(() => import('../../components/EbookForm'), { ssr:false });
-export default function EbookMaker(){ return (<main className='page'><h1>Ebook Maker</h1><EbookForm/></main>); }
