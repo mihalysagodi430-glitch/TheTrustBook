824b1242d280147afb5a26961fba254ebd36a3fe
mihalysagodi430-glitch <mihalysagodi430@gmail.com>
2025-12-24 22:37:32 +0100

    Delete Navbar.jsx

--- Navbar.jsx
+++
@@ -1,3 +0,0 @@
-'use client'
-import Link from 'next/link'
-export default function Navbar(){ return (<nav className='nav'><Link href='/home'>Home</Link> | <Link href='/pricing'>Pricing</Link></nav>); }
